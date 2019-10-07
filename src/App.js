46e84fcf3f6558ca813fa2a5ff4725941a72ed87
spatten <scott@scottpatten.ca>
2019-10-06 23:27:11 -0700

    Prevent zooming on mobile

--- src/App.js
+++ src/App.js
@@ -10,6 +10,13 @@ function App() {
     <>
       <Helmet>
         <title>Idle Component!</title>
+        // This style and the following meta tag prevent zooming in on mobile, which is really annoying when you're just clicking on buttons
+        <style type="text/css">{`
+          body {
+            touch-action: manipulation;
+          }
+        `}</style>
+        <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport"/>
       </Helmet>
       <Pane display="flex" flexDirection="column" padding={majorScale(2)}>
         <Heading>Resources</Heading>
