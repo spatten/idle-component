9b9089cfdc02e30f5902dfedacac6bf5a4e3818f
spatten <scott@scottpatten.ca>
2019-10-06 22:18:08 -0700

    Adding react-helmet and setting a title

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import React from 'react';
 import './App.css';
 import Resource from './components/resource'
 import { Pane } from 'evergreen-ui'
+import { Helmet } from 'react-helmet'
 
 function App() {
   return (
-    <Pane display="flex" flexDirection="row">
-      <Resource name="wood" />
-      <Resource name="iron" />
-      <Resource name="oil" />
-    </Pane>
+    <>
+      <Helmet>
+        <title>Idle Component!</title>
+      </Helmet>
+      <Pane display="flex" flexDirection="row">
+        <Resource name="wood" />
+        <Resource name="iron" />
+        <Resource name="oil" />
+      </Pane>
+    </>
   );
 }
 
