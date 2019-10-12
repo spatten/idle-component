fb6ec0459798d4014448aac898b5e882dd477011
spatten <scott@scottpatten.ca>
2019-10-11 19:28:10 -0700

    move initial state into its own file

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,38 +1,6 @@
 import reduceReducers from "reduce-reducers";
 import resourcesReducer from "./resourcesReducer"
 import buildingsReducer from "./buildingsReducer"
-
-const initialState = {
-  buildings: {
-    hut: {
-      slug: 'hut',
-      name: 'Hut',
-      count: 0,
-      cost: {
-        wood: 10
-      }
-    }
-  },
-  resources: {
-    wood:
-    { name: 'Wood',
-      count: 0,
-      capacity: 500,
-      icon: 'tree',
-      action: 'Chop'},
-    iron: {
-      name: 'Iron',
-      count: 0,
-      capacity: 500,
-      icon: 'mountain',
-      action: 'Mine'},
-    oil: {
-      name: 'Oil',
-      count: 0,
-      capacity: 100,
-      icon: 'oil-field',
-      action: 'Pump'},
-  }
-}
+import initialState from "./initialState"
 
 export default reduceReducers(initialState, resourcesReducer, buildingsReducer);
---
+++ src/redux/reducers/initialState.js
@@ -0,0 +1,32 @@
+export default {
+  buildings: {
+    hut: {
+      slug: 'hut',
+      name: 'Hut',
+      count: 0,
+      cost: {
+        wood: 10
+      }
+    }
+  },
+  resources: {
+    wood:
+    { name: 'Wood',
+      count: 0,
+      capacity: 500,
+      icon: 'tree',
+      action: 'Chop'},
+    iron: {
+      name: 'Iron',
+      count: 0,
+      capacity: 500,
+      icon: 'mountain',
+      action: 'Mine'},
+    oil: {
+      name: 'Oil',
+      count: 0,
+      capacity: 100,
+      icon: 'oil-field',
+      action: 'Pump'},
+  }
+}
