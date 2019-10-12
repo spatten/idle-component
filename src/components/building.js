b1041a3f4643ba4d62a9e3cd387a5bc611aceb16
spatten <scott@scottpatten.ca>
2019-10-11 21:38:31 -0700

    Configuring eslint

--- src/components/building.js
+++ src/components/building.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import { createBuilding } from '../redux/actions'
 import { majorScale, Button, Card, Text } from 'evergreen-ui'
 
-function Building({ count, name, icon, handleClick }) {
+function Building ({ count, name, icon, handleClick }) {
   return (
     <Card width={majorScale(20)}
           height={majorScale(10)}
