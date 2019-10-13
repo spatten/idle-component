1c30183d98c59ecdcb53ad795307cb84106d64bc
spatten <scott@scottpatten.ca>
2019-10-12 22:03:38 -0700

    Cleaning up a lot of leftover stuff from the create-react package and the todo tutorial

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 
-import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
