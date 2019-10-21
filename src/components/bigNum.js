b6419d26afa0866de2eb61561d6a08a0a43ad0fb
spatten <scott@scottpatten.ca>
2019-10-21 14:54:56 -0700

    BigNum goes from 10k to 999Dc, and goes to scientific notation above that

--- src/components/bigNum.js
+++ src/components/bigNum.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const suffixes = ['k', 'M', 'B', 'T', 'Qi', 'Qa', 'Sx', 'Sp']
+const suffixes = ['', 'k', 'M', 'B', 'T', 'Qi', 'Qa', 'Sx', 'Sp', 'Oc', 'No', 'Dc']
 
 function BigNum ({ number }) {
-  if (number < 1000000) {
+  if (number < 10000) {
     const nf = new Intl.NumberFormat('en')
     return <>{nf.format(number)}</>
   }
   const nf = new Intl.NumberFormat('en', { maximumSignificantDigits: 3 })
   const exp = Math.floor(Math.log10(number) / 3)
   const coefficient = nf.format(number / 10 ** (exp * 3))
-  const suffix = suffixes[exp - 1]
+  let suffix = suffixes[exp]
+  if (suffix === undefined) {
+    suffix = `e${exp * 3}`
+  }
   const numString = `${coefficient}${suffix}`
   return <>{numString}</>
 }
