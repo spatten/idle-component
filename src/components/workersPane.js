e00bf04b6184dfa98a3eb91ac211d2077baf1018
spatten <scott@scottpatten.ca>
2019-10-21 14:55:26 -0700

    Use BigNum in workers count

--- src/components/workersPane.js
+++ src/components/workersPane.js
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { majorScale, Heading, Pane, Text } from 'evergreen-ui'
 import Worker from './worker'
+import BigNum from './bigNum'
 
 function WorkersPane ({ workers }) {
   const workersCount = Object.keys(workers).reduce((s, w) => s + workers[w].count, 0)
   return (
     <Pane display="flex" flexDirection="column" padding={majorScale(2)}>
-      <Heading>Workers <Text>(total: {workersCount}, unassigned: {workers.unassigned.count})</Text></Heading>
+    <Heading>Workers <Text>(total: <BigNum number={workersCount} /> , unassigned: <BigNum number={workers.unassigned.count} />)</Text></Heading>
       <Pane display="flex" flexDirection="row" flexWrap="wrap">
         <Worker name="farmers" />
         <Worker name="woodcutters" />
